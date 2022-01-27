041d7719b9d5e840da74eb2a7c7869498e5ac7bb
Sparsh Gupta <sparshgpt18@gmail.com>
2022-01-27 21:13:54 +0530

    Add files via upload

--- Final Value of Variable After Performing Operations.cpp
+++ Final Value of Variable After Performing Operations.cpp
@@ -1,4 +1,7 @@
 // https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+// Just checked the first or last of each string as +, if it is +, then increment the number
+// else decrement it.
+// Time Complexityt - O(n), Space Complexity - O(1)
 class Solution {
 public:
     int finalValueAfterOperations(vector<string>& operations) {
@@ -6,8 +9,6 @@ public:
         for (int i = 0; i < operations.size(); i++)
             if (operations[i][0] == '+' || operations[i][2] == '+')
                 ans++;
-            else
-                ans--;
-        return ans;
+        return 2 * ans - operations.size();
     }
 };
