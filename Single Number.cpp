88b83f7c674eefdacb79031637b7151070b26f98
Sparsh Gupta <sparshgpt18@gmail.com>
2022-06-17 15:53:55 +0530

    Delete Single Number.cpp

--- Single Number.cpp
+++
@@ -1,9 +0,0 @@
-// https://leetcode.com/problems/single-number/
-class Solution {
-public:
-    int singleNumber(vector<int>& nums) {
-        for (int i = 1; i < nums.size(); i++)
-            nums[i] ^= nums[i - 1];
-        return nums[nums.size() - 1];
-    }
-};
